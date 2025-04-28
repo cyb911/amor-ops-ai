bd6b50c35a6f2f0634a9ddc05c000cc51f905034
yunbin.chen <yunbin110@gmail.com>
2025-04-28 10:47:03 +0800

    环境变量

--- chat-client/src/main/java/com/amor/chatclient/webui/WwAiAppLayout.java
+++ chat-client/src/main/java/com/amor/chatclient/webui/WwAiAppLayout.java
@@ -16,7 +16,7 @@ import com.vaadin.flow.router.PageTitle;
 
 import java.util.Map;
 
-@PageTitle("Ww AI Playground")
+@PageTitle("AMOR")
 public class WwAiAppLayout  extends AppLayout {
     private final Map<String, Class<? extends Component>> tabContents;
 
@@ -25,7 +25,7 @@ public class WwAiAppLayout  extends AppLayout {
         titleLayout.setPadding(true);
         titleLayout.setSpacing(true);
         titleLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
-        Image springImg = new Image("https://spring.io/img/projects/spring.svg", "Ww AI Playground");
+        Image springImg = new Image("https://spring.io/img/projects/spring.svg", "AMOR");
         springImg.getStyle().set("width", "var(--lumo-icon-size-m)").set("height", "var(--lumo-icon-size-m)");
         Div springImgDiv = new Div(springImg);
         springImgDiv.getStyle().set("display", "flex").set("justify-content", "center").set("align-items", "center");
@@ -33,9 +33,9 @@ public class WwAiAppLayout  extends AppLayout {
         Tabs tabs = new Tabs();
         addToNavbar(titleLayout, tabs);
 
-        this.tabContents = Map.of("大模型", ChatView.class,"矢量数据库", VectorStoreView.class);
-        tabs.add(new Tab("大模型"));
-        tabs.add(new Tab("矢量数据库"));
+        this.tabContents = Map.of("会话", ChatView.class,"运维知识库", VectorStoreView.class);
+        tabs.add(new Tab("会话"));
+        tabs.add(new Tab("运维知识库"));
         tabs.addSelectedChangeListener(
                 event -> UI.getCurrent().navigate(tabContents.get(event.getSelectedTab().getLabel())));
     }

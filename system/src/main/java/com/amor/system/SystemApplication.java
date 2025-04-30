b8475384427b678e35e9d051f42f0ed62f6ca24e
yunbin.chen <yunbin110@gmail.com>
2025-04-30 09:00:33 +0800

    新增后台管理模块

---
+++ system/src/main/java/com/amor/system/SystemApplication.java
@@ -0,0 +1,15 @@
+package com.amor.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class SystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
+
+}
